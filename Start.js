7f519a46cdcde713ca31df2f2acecd2a38e4dfa3
jeffhatch <jeffrey.hatch@gmail.com>
2017-07-06 16:37:23 -0700

    updating

--- Start.js
+++ Start.js
@@ -1,3 +1,6 @@
+var fs = require("fs");
+var inquirer = require("inquirer");
+
 const BasicCard = require('./BasicCard.js');
 const ClozeCard = require('./ClozeCard.js');
 
@@ -22,3 +25,9 @@ console.log(firstPresidentCloze.fullText);
 
 // Should throw or log an error because "oops" doesn't appear in "This doesn't work"
 var brokenCloze = new ClozeCard("This doesn't work", "oops");
+
+var fs = require("fs");
+
+// use the file system for node to allow for creation of flash cards and saving to a log.txt file
+// use inquirer to ask the user for the "front" and the "back" of the flash card they wish to create
+// use prompts such as "create" and "view" for the different logic paths
